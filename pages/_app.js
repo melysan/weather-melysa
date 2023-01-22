40a24a68b8beeff51cbd4c12e23015cdd830ebb0
MelysaNguyen <mnguyen181@my.bcit.ca>
2023-01-22 02:34:44 -0800

    change font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/globals.css'
+import { Urbanist } from '@next/font/google'
+const weatherfont = Urbanist({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={weatherfont.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
