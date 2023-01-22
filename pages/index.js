9ae0d47d4e6d7f922de629682141d1ed999508b1
MelysaNguyen <mnguyen181@my.bcit.ca>
2023-01-22 14:00:50 -0800

    more styling

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,7 @@ export default function Home() {
             />
             {errorMessage}
           </div>
-          <div className={styles.rowContainer}>
+          <div className={styles.columnContainer}>
             <div className={styles.countryName}>{data.name}</div>
             {
               weather && weather.map((w, index) => {
@@ -70,15 +70,15 @@ export default function Home() {
                 return (
 
                   <div key={index}>
-                    <div className={styles.columnContainer}>
+                    <div className={styles.justifyContainer}>
                       <Image loader={() => src} src={src} width={50} height={50} />
                       {/* <Image src={weatherIcon} width={100} height={100} /> */}
                       {/* <Image src={iconString} className={styles.weatherIcon} width={10} height={10} /> */}
                       <div className={styles.mainTemperature}>{data.main.temp} °C</div>
-                      <div className={styles.feelsLike}>{data.main.feels_like} °C</div>
+                      <div className={styles.feelsLike}>FEELS LIKE {data.main.feels_like} °C</div>
                       <div className={styles.windGust}>{data.wind.gust}</div>
                       <div className={styles.mainDescription}>{w.main}</div>
-                      <div className={styles.weatherDescription}>{w.description}</div>
+                      <div className={styles.weatherDescription}>The forecast today are {w.description}.</div>
                     </div>
                   </div>
                 )
